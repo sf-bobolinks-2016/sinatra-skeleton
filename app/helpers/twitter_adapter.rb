60e0a006101488371de6ccfcaf4f8a2173d338ce
Frank Sommers <email": null>
2016-10-10 17:04:43 -0700

    fixed issues with dotenv whitespace mattersgit statusgit status

--- app/helpers/twitter_adapter.rb
+++ app/helpers/twitter_adapter.rb
@@ -1,16 +1,22 @@
-client = Twitter::Streaming::Client.new do |config|
-  config.consumer_key = ENV['CONSUMER_KEY']
-  config.consumer_secret = ENV['CONSUMER_SECRET']
-  config.access_token =  ENV['ACCESS_TOKEN']
-  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+def TwitterCall
+
+  client = Twitter::Streaming::Client.new do |config|
+    config.consumer_key = ENV['CONSUMER_KEY']
+    config.consumer_secret = ENV['CONSUMER_SECRET']
+    config.access_token =  ENV['ACCESS_TOKEN']
+    config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+  end
+
+  def trump_talk
+    client.filter({:follow => '25073877'}) do |object|
+      puts object.text if object.is_a?(Twitter::Tweet)
+       end
+  end
+
+  def trump_stream
+    client.user({:with => '25073877'}) do |object|
+      puts object.text if object.is_a?(Twitter::Tweet)
+     end
+  end
 end
 
-def trump_talk
-  client.filter({:follow => '25073877'}) do |object|
-    puts object.text if object.is_a?(Twitter::Tweet)
-end
-
-def trump_stream
-  client.user({:with => '25073877'}) do |object|
-    puts object.text if object.is_a?(Twitter::tweet)
-end
