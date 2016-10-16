886ed11cfb5919f9c2d5837f5c8e6dd5b5edf5ce
Frank Sommers <ssommers21@gmail.com>
2016-10-16 16:44:44 -0700

    adds cleaned up navbar and css to style it

--- db/migrate/20161015174341_create_trumps.rb
+++ db/migrate/20161015174341_create_trumps.rb
@@ -1,4 +1,9 @@
 class CreateTrumps < ActiveRecord::Migration
   def change
+  	 create_table :trumps do |t|
+      t.string :body
+      t.string :username 
+      t.timestamps
+  	 end	
   end
 end
--- db/migrate/20161015174358_create_clintons.rb
+++ db/migrate/20161015174358_create_clintons.rb
@@ -1,7 +1,9 @@
 class CreateClintons < ActiveRecord::Migration
   def change
-    create_table do |t|
-     t.string = :tweet
-    end
+   	 create_table :clintons do |t|
+      t.string :body
+      t.string :username 
+      t.timestamps
+  	 end	
   end
 end
